ffe9df3410ec76b75d5d61d40ed37255ec37455c
Rachel Vander Vliet <rvandervliet@stsci.edu>
2024-04-04 11:39:02 -0400

    Testing init settings

--- {{ cookiecutter.package_name }}/setup.py
+++ {{ cookiecutter.package_name }}/setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-{%- if cookiecutter.use_compiled_extensions == 'y' %}
+{%- if cookiecutter._use_compiled_extensions == 'y' %}
 from extension_helpers import get_extensions
 {%- endif %}
 from setuptools import setup
 
-setup( {%- if cookiecutter.use_compiled_extensions == 'y' -%}
+setup( {%- if cookiecutter._use_compiled_extensions == 'y' -%}
     ext_modules=get_extensions()
 {%- endif -%} )
